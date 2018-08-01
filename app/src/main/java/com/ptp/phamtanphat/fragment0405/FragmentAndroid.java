1e80e4569847288be9797703399e4bdf2f6a50b5
thuviengithub0208 <khoaphp@yahoo.com>
2018-08-01 17:58:19 +0700

    "Fragment"

--- app/src/main/java/com/ptp/phamtanphat/fragment0405/FragmentAndroid.java
+++ app/src/main/java/com/ptp/phamtanphat/fragment0405/FragmentAndroid.java
@@ -1,4 +1,22 @@
 package com.ptp.phamtanphat.fragment0405;
 
-public class FragmentAndroid {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentAndroid extends Fragment {
+
+
+    //Set layout cho fragment android
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_android,container,false);
+        return view;
+    }
 }
